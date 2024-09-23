5c14a4e9b957310158b36bb4bd6570eea9bb9a23
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2024-09-23 09:58:37 -0600

    added exponential example

---
+++ examples/exp_example.py
@@ -0,0 +1,13 @@
+from goph419.functions import exp
+
+
+def main():
+    x = 1.0
+    print(f"exp({x}) = {exp(x)}")
+
+    x = 2.0
+    print(f"exp({x}) = {exp(x)}")
+
+
+if __name__ == "__main__":
+    main()
