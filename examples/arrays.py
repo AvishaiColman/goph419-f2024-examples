2c620e2d53ffb790ca297fee79c18dffe6c1d0b0
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2024-10-22 13:54:39 -0600

    updated example to test forward substitution

--- examples/arrays.py
+++ examples/arrays.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from goph419.linalg import forward_substitution
+
 
 def main():
     A = np.array(
@@ -12,7 +14,9 @@ def main():
     print(f"b: {b}")
 
     x_np = np.linalg.solve(A, b)
+    x_for = forward_substitution(A, b)
     print(f"x_np: {x_np}")
+    print(f"x_for: {x_for}")
 
 
 if __name__ == "__main__":
