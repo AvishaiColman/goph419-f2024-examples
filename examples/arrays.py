adc06d3d1116806fa918818593c69beb23b034fa
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2024-10-28 09:49:12 -0600

    added backward substitution function and example

--- examples/arrays.py
+++ examples/arrays.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-from goph419.linalg import forward_substitution
+from goph419.linalg import (
+    forward_substitution,
+    backward_substitution,
+)
 
 
 def main():
+    # testing a lower triangular system
     A = np.array(
         [[8e5, 0, 0, 0], [-8e5, 2e6, 0, 0], [0, -2e6, 8e6, 0], [0, 0, -8e6, 2e7]]
     )
     b = np.array([1.0, 2.0, 3.0, 4.0])
 
-    print("solving A * x = b")
+    print("solving A * x = b with forward substitution")
     print(f"A: {A}")
     print(f"b: {b}")
 
@@ -17,6 +21,20 @@ def main():
     x_for = forward_substitution(A, b)
     print(f"x_np: {x_np}")
     print(f"x_for: {x_for}")
+
+    # testing an upper triangular system
+    A = np.array([[4, -1, -1, -1], [0, 5, -1, -2], [0, 0, 2, -1], [0, 0, 0, 8]])
+    b = np.array([[1.0, 2.0, 3.0, 4.0], [2.0, 4.0, 6.0, 8.0]]).T
+
+    print()
+    print("solving A * x = b with backward substitution")
+    print(f"A: {A}")
+    print(f"b: {b}")
+
+    x_np = np.linalg.solve(A, b)
+    x_for = backward_substitution(A, b)
+    print(f"x_np: {x_np}")
+    print(f"x_back: {x_for}")
 
 
 if __name__ == "__main__":
--- src/goph419/linalg.py
+++ src/goph419/linalg.py
@@ -55,3 +55,59 @@ def forward_substitution(a, b):
     if b_one_d:
         x = x.flatten()
     return x
+
+
+def backward_substitution(a, b):
+    """Solve an upper triangular system a * x = b
+    for x given a and b.
+
+    Parameters
+    ----------
+    a : array_like, shape=(M, M)
+        The coefficient matrix, in upper triangular form.
+    b : array_like, shape=(M, ) or (M, N)
+        The right-hand side vector. Can solve for multiple
+        right-hand sides if b is 2 dimensional.
+
+    Returns
+    -------
+    x : numpy.ndarray, shape=(M, ) or (M, N)
+        The solution vector(s). Will match the shape of b.
+
+    Notes
+    -----
+    Assume that a is in upper triangular form and that all
+    values below the main diagonal are 0. These values are not
+    used in the algorithm, so other information can be safely
+    stored there without affecting output.
+    """
+    a = np.array(a, dtype="float64")
+    b = np.array(b, dtype="float64")
+    a_shape = a.shape
+    M = a_shape[0]
+    if len(a_shape) != 2:
+        raise ValueError(
+            f"coefficient matrix has dimension {len(a_shape)}, should be 2."
+        )
+    if M != a_shape[1]:
+        raise ValueError(f"a has shape {a_shape}, should be square.")
+    b_shape = b.shape
+    if len(b_shape) < 1 or len(b_shape) > 2:
+        raise ValueError(f"b has dimension {len(b_shape)}, should be 1 or 2.")
+    if M != b_shape[0]:
+        raise ValueError(
+            f"b has leading dimension {b_shape[0]}, should match leading dimension of a which is {M}"
+        )
+    b_one_d = len(b_shape) == 1
+    # convert b to (M, 1) for one dimensional b
+    # we will put it back to one dimensional later
+    if b_one_d:
+        b = np.reshape(b, (M, 1))
+    # backward substitution algorithm
+    x = np.zeros_like(b)
+    for k in range(-1, -(M + 1), -1):
+        x[k, :] = (b[k, :] - a[k, (k + 1) :] @ x[(k + 1) :, :]) / a[k, k]
+    # tidy up output shape
+    if b_one_d:
+        x = x.flatten()
+    return x
